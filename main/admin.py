38f46807072b1b426a8c8bd6120f4d631719431f
Nissel21 <71747645+Nissel21@users.noreply.github.com>
2020-10-04 23:23:48 -0500

    first commit

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
 
-from .models import Localizacion, Producto, Categoria, Proveedor
+from .models import *
 #tambien se puede importar todas las clases con *
 
+#....
+...
+class ClienteInline(admin.TabularInline):
+    model=Cliente
+
+
+class ColaboradorInline(admin.TabularInline):
+    model=Colaborador
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    inlines = [
+        ClienteInline,
+        ColaboradorInline
+    ]
+
 # Register your models here.
+...
+admin.site.register(Cliente)
+admin.site.register(Colaborador)
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Localizacion)
 admin.site.register(Producto)
 admin.site.register(Categoria)
